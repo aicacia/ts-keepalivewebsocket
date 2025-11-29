6877d20a72fc18680596dd5e3d40e4215f93bf6c
Nathan Faucett <nathanfaucett@gmail.com>
2025-11-29 07:46:51 -0500

    add binaryType

--- src/KeepAliveWebSocket.test.ts
+++ src/KeepAliveWebSocket.test.ts
@@ -1,58 +1,68 @@
 import tape from "tape";
 import {
-	server as WebSocketServer,
-	w3cwebsocket as WebSocket,
+  server as WebSocketServer,
+  w3cwebsocket as WebSocket,
 } from "websocket";
 import { createServer, type Server } from "node:http";
 import { KeepAliveWebSocket } from "./KeepAliveWebSocket";
 
 tape("basic", async (assert: tape.Test) => {
-	let websocket: KeepAliveWebSocket | undefined;
-	let httpServer: Server | undefined;
-	let wsServer: WebSocketServer | undefined;
-	try {
-		httpServer = createServer((request, response) => {
-			response.writeHead(404);
-			response.end();
-		});
-		httpServer.listen(8080);
-		wsServer = new WebSocketServer({
-			httpServer,
-			autoAcceptConnections: true,
-		});
-		wsServer.on("connect", (connection) => {
-			connection.on("message", (message) => {
-				if (message.type === "utf8") {
-					connection.sendUTF(message.utf8Data);
-				}
-			});
-		});
+  let websocket: KeepAliveWebSocket | undefined;
+  let httpServer: Server | undefined;
+  let wsServer: WebSocketServer | undefined;
+  try {
+    httpServer = createServer((request, response) => {
+      console.log("HTTP request received");
+      response.writeHead(404);
+      response.end();
+    });
+    httpServer.listen(7823);
+    console.log("Listening on port 7823");
+    wsServer = new WebSocketServer({
+      httpServer,
+      autoAcceptConnections: true,
+    });
+    wsServer.on("connect", (connection) => {
+      console.log("WebSocket client connected");
+      connection.on("message", (message) => {
+        console.log("WebSocket client sent a message");
+        if (message.type === "utf8") {
+          connection.sendUTF(message.utf8Data);
+        }
+      });
+    });
+    console.log("WebSocket server created");
 
-		websocket = new KeepAliveWebSocket({
-			url: () => "ws://localhost:8080",
-			WebSocket: WebSocket as never,
-			autoconnect: true,
-			minTimeBetweenReconnectsMS: 1000,
-		});
+    websocket = new KeepAliveWebSocket({
+      url: () => "ws://localhost:7823",
+      WebSocket: WebSocket as never,
+      autoconnect: true,
+      minTimeBetweenReconnectsMS: 1000,
+    });
+    console.log("KeepAliveWebSocket created");
 
-		const message1Promise = websocket.message();
-		await websocket.ready();
-		websocket.send("Hello");
-		assert.equal(await message1Promise, "Hello");
+    const message1Promise = websocket.message();
+    console.log("Waiting Websocket ready");
+    await websocket.ready();
+    console.log("Websocket ready");
+    websocket.send("Hello");
+    console.log("Sent first message");
+    assert.equal(await message1Promise, "Hello");
+    console.log("Received first message");
 
-		const disconnectPromise = websocket.waitOnce("disconnect");
-		wsServer.closeAllConnections();
-		await disconnectPromise;
+    const disconnectPromise = websocket.waitOnce("disconnect");
+    wsServer.closeAllConnections();
+    await disconnectPromise;
 
-		const message2Promise = websocket.message();
-		await websocket.ready();
-		websocket.send("world!");
-		assert.equal(await message2Promise, "world!");
+    const message2Promise = websocket.message();
+    await websocket.ready();
+    websocket.send("world!");
+    assert.equal(await message2Promise, "world!");
 
-		assert.end();
-	} finally {
-		httpServer?.close();
-		wsServer?.closeAllConnections();
-		websocket?.close();
-	}
+    assert.end();
+  } finally {
+    websocket?.close();
+    wsServer?.closeAllConnections();
+    httpServer?.close();
+  }
 });
--- src/KeepAliveWebSocket.ts
+++ src/KeepAliveWebSocket.ts
@@ -1,174 +1,206 @@
 import {
-	EventEmitter,
-	type EventEmitter as EventEmitterTypes,
+  EventEmitter,
+  type EventEmitter as EventEmitterTypes,
 } from "eventemitter3";
 
 export type KeepAliveWebSocketEvents = {
-	open(this: KeepAliveWebSocket): void;
-	message(this: KeepAliveWebSocket, data: string | ArrayBufferLike | Blob | ArrayBufferView): void;
-	error(this: KeepAliveWebSocket, error?: Error): void;
-	disconnect(this: KeepAliveWebSocket): void;
-	close(this: KeepAliveWebSocket): void;
+  open(this: KeepAliveWebSocket): void;
+  message(
+    this: KeepAliveWebSocket,
+    data: string | ArrayBufferLike | Blob | ArrayBufferView
+  ): void;
+  error(this: KeepAliveWebSocket, error?: Error): void;
+  disconnect(this: KeepAliveWebSocket): void;
+  close(this: KeepAliveWebSocket): void;
 };
 
 type KeepAliveWebSocketEventNames =
-	EventEmitterTypes.EventNames<KeepAliveWebSocketEvents>;
+  EventEmitterTypes.EventNames<KeepAliveWebSocketEvents>;
 type KeepAliveWebSocketEventArguments =
-	EventEmitterTypes.ArgumentMap<KeepAliveWebSocketEvents>;
+  EventEmitterTypes.ArgumentMap<KeepAliveWebSocketEvents>;
 type EventEmitterReturnType<T> = T extends []
-	? // biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
-	void
-	: T extends [infer R]
-	? R
-	: T;
+  ? // biome-ignore lint/suspicious/noConfusingVoidType: <explanation>
+    void
+  : T extends [infer R]
+  ? R
+  : T;
 
 export type KeepAliveWebSocketOptions = {
-	url: () => Promise<string> | string;
-	minTimeBetweenReconnectsMS?: number;
-	autoconnect?: boolean;
-	WebSocket?: typeof WebSocket;
+  url: () => Promise<string> | string;
+  minTimeBetweenReconnectsMS?: number;
+  autoconnect?: boolean;
+  binaryType?: "blob" | "arraybuffer";
+  WebSocket?: typeof WebSocket;
 };
 
 export class KeepAliveWebSocket extends EventEmitter<KeepAliveWebSocketEvents> {
-	private url: () => Promise<string> | string;
-	private connected = false;
-	private connecting = false;
-	private reconnecting = false;
-	private isClosed = false;
-	private websocket: WebSocket | undefined;
-	private connectTime = Date.now();
-	private minTimeBetweenReconnectsMS = 0;
-	private WebSocket: typeof WebSocket;
+  private url: () => Promise<string> | string;
+  private connected = false;
+  private connecting = false;
+  private reconnecting = false;
+  private isClosed = false;
+  private websocket: WebSocket | undefined;
+  private connectTime = Date.now();
+  private minTimeBetweenReconnectsMS = 0;
+  private binaryType: "blob" | "arraybuffer" | undefined = undefined;
+  private WebSocket: typeof WebSocket;
 
-	constructor(options: KeepAliveWebSocketOptions) {
-		super();
-		this.url = options.url;
-		if (options.WebSocket) {
-			this.WebSocket = options.WebSocket;
-		} else {
-			this.WebSocket = WebSocket;
-		}
-		if (options.minTimeBetweenReconnectsMS) {
-			this.minTimeBetweenReconnectsMS = options.minTimeBetweenReconnectsMS;
-		}
-		if (options.autoconnect) {
-			this.connect();
-		}
-	}
+  constructor(options: KeepAliveWebSocketOptions) {
+    super();
+    this.url = options.url;
+    if (options.WebSocket) {
+      this.WebSocket = options.WebSocket;
+    } else {
+      this.WebSocket = WebSocket;
+    }
+    if (options.minTimeBetweenReconnectsMS) {
+      this.minTimeBetweenReconnectsMS = options.minTimeBetweenReconnectsMS;
+    }
+    if (options.binaryType) {
+      this.binaryType = options.binaryType;
+    }
+    if (options.autoconnect) {
+      this.connect();
+    }
+  }
 
-	async send(data: string | ArrayBufferLike | Blob | ArrayBufferView) {
-		await this.ready();
-		// biome-ignore lint/style/noNonNullAssertion: ready
-		this.websocket!.send(data);
-		return this;
-	}
+  getWebSocket() {
+    return this.websocket;
+  }
 
-	async ready() {
-		if (!this.connected) {
-			await this.waitOnce("open");
-		}
-		return this;
-	}
+  setUrl(url: () => Promise<string> | string) {
+    this.url = url;
+    this.close();
+    return this.connect();
+  }
 
-	message() {
-		return this.waitOnce("message");
-	}
+  setBinaryType(binaryType: "blob" | "arraybuffer" | undefined) {
+    this.binaryType = binaryType;
+    if (this.websocket && binaryType) {
+      this.websocket.binaryType = binaryType;
+    }
+    return this;
+  }
 
-	waitOnce<K extends KeepAliveWebSocketEventNames>(event: K) {
-		return new Promise<
-			EventEmitterReturnType<KeepAliveWebSocketEventArguments[K]>
-		>((resolve) => {
-			this.once(event, (...args) => {
-				switch (args.length) {
-					case 0:
-						resolve(undefined as never);
-						break;
-					case 1:
-						resolve(args[0]);
-						break;
-					default:
-						resolve(args as never);
-						break;
-				}
-			});
-		});
-	}
+  async send(data: string | ArrayBufferLike | Blob | ArrayBufferView) {
+    await this.ready();
+    // biome-ignore lint/style/noNonNullAssertion: ready
+    this.websocket!.send(data);
+    return this;
+  }
 
-	close(code?: number, reason?: string) {
-		this.connected = false;
-		this.connecting = false;
-		this.isClosed = true;
-		if (this.websocket) {
-			this.websocket.close(code, reason);
-		} else {
-			this.emit("close");
-		}
-	}
+  async ready() {
+    if (!this.connected) {
+      await this.waitOnce("open");
+    }
+    return this;
+  }
 
-	async connect() {
-		if (this.connected) {
-			return this;
-		}
-		if (this.connecting) {
-			return this;
-		}
-		this.connecting = true;
-		try {
-			this.connectTime = Date.now();
-			const websocket = new this.WebSocket(await this.url());
+  message() {
+    return this.waitOnce("message");
+  }
 
-			const onOpen = () => {
-				websocket.removeEventListener("open", onOpen);
-				this.connected = true;
-				this.emit("open");
-			};
-			websocket.addEventListener("open", onOpen);
+  waitOnce<K extends KeepAliveWebSocketEventNames>(event: K) {
+    return new Promise<
+      EventEmitterReturnType<KeepAliveWebSocketEventArguments[K]>
+    >((resolve) => {
+      this.once(event, (...args) => {
+        switch (args.length) {
+          case 0:
+            resolve(undefined as never);
+            break;
+          case 1:
+            resolve(args[0]);
+            break;
+          default:
+            resolve(args as never);
+            break;
+        }
+      });
+    });
+  }
 
-			websocket.addEventListener("close", () => {
-				this.websocket = undefined;
-				this.connected = false;
-				if (this.isClosed) {
-					this.emit("close");
-				} else {
-					this.emit("disconnect");
-					this.reconnect();
-				}
-			});
-			websocket.addEventListener("message", (event) => {
-				this.emit("message", event.data);
-			});
-			websocket.addEventListener("error", () => {
-				this.emit("error");
-			});
+  close(code?: number, reason?: string) {
+    this.connected = false;
+    this.connecting = false;
+    this.isClosed = true;
+    if (this.websocket) {
+      this.websocket.close(code, reason);
+    } else {
+      this.emit("close");
+    }
+    return this;
+  }
 
-			this.websocket = websocket;
-		} catch (error) {
-			this.emit("error", error as Error);
-			await this.reconnect();
-		} finally {
-			this.connecting = false;
-		}
-		return this;
-	}
+  async connect() {
+    if (this.connected) {
+      return this;
+    }
+    if (this.connecting) {
+      return this;
+    }
+    this.isClosed = false;
+    this.connecting = true;
+    try {
+      this.connectTime = Date.now();
+      const websocket = new this.WebSocket(await this.url());
 
-	private async reconnect() {
-		if (this.reconnecting) {
-			return this;
-		}
-		this.reconnecting = true;
-		try {
-			const timeSinceLastConnect = Date.now() - this.connectTime;
-			if (timeSinceLastConnect < this.minTimeBetweenReconnectsMS) {
-				await waitMS(this.minTimeBetweenReconnectsMS - timeSinceLastConnect);
-			}
-			await this.connect();
-		} finally {
-			this.reconnecting = false;
-		}
-		return this;
-	}
+      if (this.binaryType) {
+        websocket.binaryType = this.binaryType;
+      }
+
+      const onOpen = () => {
+        websocket.removeEventListener("open", onOpen);
+        this.connected = true;
+        this.emit("open");
+      };
+      websocket.addEventListener("open", onOpen);
+
+      websocket.addEventListener("close", () => {
+        this.websocket = undefined;
+        this.connected = false;
+        if (this.isClosed) {
+          this.emit("close");
+        } else {
+          this.emit("disconnect");
+          this.reconnect();
+        }
+      });
+      websocket.addEventListener("message", (event) => {
+        this.emit("message", event.data);
+      });
+      websocket.addEventListener("error", () => {
+        this.emit("error");
+      });
+
+      this.websocket = websocket;
+    } catch (error) {
+      this.emit("error", error as Error);
+      await this.reconnect();
+    } finally {
+      this.connecting = false;
+    }
+    return this;
+  }
+
+  private async reconnect() {
+    if (this.reconnecting) {
+      return this;
+    }
+    this.reconnecting = true;
+    try {
+      const timeSinceLastConnect = Date.now() - this.connectTime;
+      if (timeSinceLastConnect < this.minTimeBetweenReconnectsMS) {
+        await waitMS(this.minTimeBetweenReconnectsMS - timeSinceLastConnect);
+      }
+      await this.connect();
+    } finally {
+      this.reconnecting = false;
+    }
+    return this;
+  }
 }
 
 function waitMS(ms: number) {
-	return new Promise<void>((resolve) => setTimeout(resolve, ms));
+  return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
